efbf7dfb3c3a5e1e9e0c2d6c4b4d27d116543779
Andrew Roth <aroth21@mac.com>
2021-08-25 16:24:51 -0400

    added Notification trait.  Bug: save not working, is event firing?

--- app/Models/User.php
+++ app/Models/User.php
@@ -256,6 +256,19 @@ class User extends SnipeModel implements AuthenticatableContract, AuthorizableCo
         return $this->endpoint;
     }
 
+    /**
+     * The url for MS Teams notifications.
+     * Used by Notifiable trait.
+     * @return mixed
+     */
+    public function routeNotificationForMSTeams()
+    {
+        // At this point the endpoint is the same for everything.
+        //  In the future this may want to be adapted for individual notifications.
+        $this->endpoint = \App\Models\Setting::getSettings()->msteams_endpoint;
+        return $this->endpoint;
+    }
+
 
     /**
      * Establishes the user -> assets relationship
