43ab24dad0133a56188a1b8a3010bd1a2ea439c1
Andrew Roth <aroth21@mac.com>
2021-11-15 09:30:08 -0500

    database migration for adding codes to license seats

---
+++ database/migrations/2021_11_12_102956_add_codes_to_license_seats.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCodesToLicenseSeats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('license_seats', function ($table) {
+            $table->text('codes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('license_seats', function ($table) {
+            $table->dropColumn('codes');
+        });
+    }
+}
